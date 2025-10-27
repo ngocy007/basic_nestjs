bf78af8875fa434577445f95201fcc0ac9926d36
ngocy007 <dev.yngoc@gmail.com>
2025-10-27 16:44:31 +0700

    Update create-user.dto.ts

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,8 +1,19 @@
-import { IsEmail, IsOptional, IsString, IsEnum } from 'class-validator';
+import { IsEmail, IsOptional, IsString, IsEnum, MinLength } from 'class-validator';
 import { Role } from '../../generated/prisma/enums';
 
 export class CreateUserDto {
-  @IsEmail() email!: string;
-  @IsOptional() @IsString() name?: string;
-  @IsOptional() @IsEnum(Role) role?: Role;
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @MinLength(8)
+  password!: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsEnum(Role)
+  role?: Role;
 }
